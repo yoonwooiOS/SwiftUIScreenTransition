1088371401dd53423da064d20f525f43237226fa
yoonwooiOS <yoonwoo4429@kakao.com>
2024-09-02 14:17:27 +0900

    🎉 init : Initial Commit

---
+++ SwiftUIScreenTransition/OnBoardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoardingView.swift
+//  SwiftUIScreenTransition
+//
+//  Created by 김윤우 on 9/2/24.
+//
+
+import SwiftUI
+
+struct OnBoardingView: View {
+    var body: some View {
+       Text("")
+    }
+}
+
+#Preview {
+    OnBoardingView()
+}
