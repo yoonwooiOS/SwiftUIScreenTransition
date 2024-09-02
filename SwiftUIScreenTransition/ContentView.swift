2a44d9900c46083520a5fc325b87d8ea289e1848
yoonwooiOS <yoonwoo4429@kakao.com>
2024-09-02 13:51:08 +0900

    Initial Commit

---
+++ SwiftUIScreenTransition/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIScreenTransition
+//
+//  Created by 김윤우 on 9/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIScreenTransition/SwiftUIScreenTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIScreenTransitionApp.swift
+//  SwiftUIScreenTransition
+//
+//  Created by 김윤우 on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIScreenTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
