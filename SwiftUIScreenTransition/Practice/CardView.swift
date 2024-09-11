a9ff35e10db2a303834ea10289cc3cc211894acb
yoonwooiOS <yoonwoo4429@kakao.com>
2024-09-11 23:48:53 +0900

    ✨ feat : HomeTabView, CardView 추가

---
+++ SwiftUIScreenTransition/Practice/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  SwiftUIScreenTransition
+//
+//  Created by 김윤우 on 9/11/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
---
+++ SwiftUIScreenTransition/View/TabView/HomeTabView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeTabView.swift
+//  SwiftUIScreenTransition
+//
+//  Created by 김윤우 on 9/10/24.
+//
+
+import SwiftUI
+
+struct HomeTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeTabView()
+}
